466a558c96a72dcdfead2e2338bd650d2fdd7183
naneve <includenaneve@outlook.com>
2019-01-27 22:00:13 +0800

    测试cookie

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,6 +2,29 @@ import React, { Component } from 'react'
 
 import './Home.less'
 export default class Test extends Component {
+
+  setCookie(c_name, value, expiredays) {
+    const deadDay = new Date(new Date().setDate(new Date().getDate() + 7))
+    document.cookie = c_name + "=" + escape(value) + ((expiredays == null) ? "" : ";expires=" + deadDay.toGMTString());
+  };
+
+  getCookie(c_name) {
+    if(document.cookie.includes('tigerGameId')) {
+      let tigerId = document.cookie.split('; ').filter(item => item.includes('tigerGameId'))[0].split('=')[1]
+      return tigerId
+    }
+    return "";
+  }
+
+  componentDidMount() {
+    if (this.getCookie() !== '') {
+      alert('欢迎' + this.getCookie())
+    } else {
+      const id = prompt('检测到你未登陆，请登录（英文）')
+      this.setCookie("tigerGameId", id, 30)
+    }
+  }
+
   render() {
     return (
       <div className="home">HOME</div>
